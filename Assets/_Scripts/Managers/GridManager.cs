444083fefaa850965588e439cc9d4eb41471add1
kopp20 <56162079+kopp20@users.noreply.github.com>
2022-05-09 23:35:12 +0200

    updated version

--- Assets/_Scripts/Managers/GridManager.cs
+++ Assets/_Scripts/Managers/GridManager.cs
@@ -7,7 +7,7 @@ public class GridManager : MonoBehaviour
     public static GridManager Instance;
     [SerializeField] private int _width, _height;
  
-    [SerializeField] private Tile _grassTile, _mountainTile;
+    [SerializeField] private Tile _tilePrefab;
    
     [SerializeField] private Transform _cam;
 
@@ -23,8 +23,8 @@ private Dictionary<Vector2, Tile> _tiles;
         for (int x = 0; x < _width; x++) 
         {
             for (int y = 0; y < _height; y++) {
-                var randomTile = Random.Range(0, 6) == 3? _mountainTile : _grassTile;
-                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
+                
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
 
               
@@ -36,7 +36,7 @@ private Dictionary<Vector2, Tile> _tiles;
         }
         _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height / 2 - 0.5f,-10);
 
-        GameManager.Instance.ChangeState(GameState.SpawnHeroes);
+     
     }
       public Tile GetTileAtPosition(Vector2 pos) {
         if (_tiles.TryGetValue(pos, out var tile)) return tile;
--- Assets/_Scripts/Tiles/Tile.cs
+++ Assets/_Scripts/Tiles/Tile.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Tile : MonoBehaviour {
+public abstract class Tile : MonoBehaviour
+{
 
     [SerializeField] protected SpriteRenderer _renderer;
     [SerializeField] private GameObject _highlight;
- 
-    public virtual void Init(int x, int y) 
+    Tile tile;
+    public virtual void Init(int x, int y)
     {
-    
+
     }
- 
-    void OnMouseEnter() {
+
+    void OnMouseEnter()
+    {
         _highlight.SetActive(true);
+
     }
- 
+
+    void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.name);
+    }
+
     void OnMouseExit()
     {
         _highlight.SetActive(false);
